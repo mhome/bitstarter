3d59ff70ae8e665d0e7a61444e406e793ee5e248
Ubuntu <ubuntu@ip-172-31-35-252.us-west-2.compute.internal>
2013-07-14 11:22:51 +0000

    First attempt - buffers and fs

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+    var buff = new Buffer(32);
+    buff = fs.writeFile("index.html","Buffer");
+    response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
